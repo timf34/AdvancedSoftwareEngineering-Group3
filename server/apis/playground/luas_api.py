0de7fe329d2ef8956223a6490a744570c33f47ac
kahern7 <kahern@tcd.ie>
2024-11-20 15:32:48 +0000

    Base layout for API classes

--- server/apis/playground/luas_api.py
+++ server/apis/playground/luas_api.py
@@ -3,32 +3,39 @@ from xml.dom.minidom import parseString
 import time
 import datetime
 
-# URL of Open Data
-STOP = 'ran'
-url = f'http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop={STOP}&encrypt=false'
+class luasAPI:
 
-# poll_interval = 10
+    def __init__(self):
+        pass
 
-# while True:
-#     # Fetch the geoJSON data from the URL
-#     response = requests.get(url)
+    def get(self, stop):
+        # URL of Open Data
+        self.stop = stop
+        self.url = f'http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop={self.stop}&encrypt=false'
 
-#     # Check if the request was successful
-#     if response.status_code == 200:
-#         for item in response.json():
-#             print(item['name'], ' ', item['available_bikes'], (int(item['last_update'])))
+        # poll_interval = 10
 
-#     else:
-#         print("Failed to retrieve data:", response.status_code)
+        # while True:
+        #     # Fetch the geoJSON data from the URL
+        #     response = requests.get(url)
 
-#     time.sleep(poll_interval)
+        #     # Check if the request was successful
+        #     if response.status_code == 200:
+        #         for item in response.json():
+        #             print(item['name'], ' ', item['available_bikes'], (int(item['last_update'])))
 
-#while True:
-# fetch the real-time Luas data from the URL
-response = requests.get(url)
+        #     else:
+        #         print("Failed to retrieve data:", response.status_code)
 
-# check if the request was succesful
-if response.status_code == 200:
-    doc = parseString(response.content)
-    # TODO: complete xml code
-    print(doc.getElementsByTagName('')[0].firstChild.nodeValue)
+        #     time.sleep(poll_interval)
+
+        #while True:
+        # fetch the real-time Luas data from the URL
+        self.response = requests.get(self.url)
+
+        # check if the request was succesful
+        if self.response.status_code == 200:
+            doc = parseString(self.response.content)
+            # TODO: complete xml code
+            print(doc.getElementsByTagName('')[0].firstChild.nodeValue)
+
