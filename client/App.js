96ba418a8dd0ffbe177160bc06d3cc3142d59209
Jason Boutlas <jasonboutlas@Jasons-MacBook-Air-2.local>
2024-11-20 13:58:27 +0000

    formatting for username and password

--- client/App.js
+++ client/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Button, TouchableOpacity, Text } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './LogIn';
 import MapScreen from './Map';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // const initialRegion = {
-  //   latitude: 53.3498,      // Dublin's latitude
-  //   longitude: -6.2603,     // Dublin's longitude
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // };
-
-  // return (
-  //   <View style={styles.container}>
-  //     <MapView style={styles.map} initialRegion={initialRegion}>
-  //       {/* Add a marker */}
-  //       <Marker
-  //         coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-  //         title="My Marker"
-  //         description="Marker description"
-  //       />
-  //     </MapView>
-  //     <TouchableOpacity style={styles.TouchableOpacity} onPress={() => console.log('Button has been pressed!')}
-  //     color="#841584">
-  //     <Text>Log In</Text>
-  //    </TouchableOpacity>
-  //   </View>
-  // );
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Map">
@@ -42,33 +16,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   map: {
-//     flex: 1,
-//   },
-//   TouchableOpacity: {
-//     position: 'absolute', // Positioning over the map
-//     alignItems: 'center',
-//     left: '70%',
-//     top: '50%',
-//     backgroundColor: '#007bff',
-//     paddingVertical: 10,
-//     paddingHorizontal: 40,
-//     borderRadius: 10,
-//     shadowColor: '#000',
-//     shadowOpacity: 0.2,
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowRadius: 5,
-//     elevation: 5,
-//   },
-//   TouchableOpacityText: {
-//     color: '#fff',
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-
-// });
--- client/LogIn.js
+++ client/LogIn.js
@@ -1,84 +1,78 @@
 import * as React from 'react';
-import {useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { useState } from 'react';
 import { StyleSheet, View, SafeAreaView, TextInput, Button, TouchableOpacity, Text } from 'react-native';
 
 
-export default function LoginScreen({navigation}) {
+export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-const handleLogin = () => {
-  if (username == null || password == null) {
-    alert("All fields have to be filled before logging in!")
-  }
-  console.log('username: ', username)
-  console.log('password: ', password)
-  alert(`Username: ${username}\nPassword: ${password}`);
-  navigation.goBack();
-};
+  const handleLogin = () => {
+    if (username == '' || password == '') {
+      alert("All fields have to be filled before logging in!")
+    }
+    else {
+      console.log('username: ', username)
+      console.log('password: ', password)
+      alert(`Username: ${username}\nPassword: ${password}`)
+      navigation.goBack()
+    }
+  };
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {/* <Text>Login Screen</Text> */}
-        {/* <Button title="Go Back" onPress={() => navigation.goBack()} /> */}
-  
-        <SafeAreaView style={styles.container}>
-            <TextInput
-                placeholder="Username"
-                value={username}
-                onChangeText={(text) => setUsername(text)}
-                // onSubmitEditing={() => alert(`Welcome to ${message}`)}
-                style={styles.TextInput}
-            />
-        </SafeAreaView>
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-        <SafeAreaView style={styles.container}>
-            <TextInput
-                placeholder="Password"
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-                // onSubmitEditing={() => alert(`You have logged in`)}
-                secureTextEntry
-                style={styles.TextInput}
-            />
-        </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          style={styles.TextInput}
+        />
 
-        <TouchableOpacity style={styles.TouchableOpacity} onPress={handleLogin}
-          color="#841584">
-          <Text>Log In</Text>
-         </TouchableOpacity>
-      </View>
-    );
-  }
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          style={styles.TextInput}
+        />
+      </SafeAreaView>
+
+      <TouchableOpacity style={styles.TouchableOpacity} onPress={handleLogin}
+        color="#841584">
+        <Text>Log In</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     paddingVertical: 10,
     paddingHorizontal: 45,
+    top: '-20%',
   },
   TextInput: {
-    width: 100, // Makes both inputs the same width
-    height: 50, // Sets identical height for both inputs
+    width: 100,
+    height: 50,
     backgroundColor: '#fff',
-    borderColor: '#ccc', // Light gray border
+    borderColor: '#ccc',
     borderWidth: 1,
-    borderRadius: 10, // Rounded corners
-    paddingHorizontal: 10, // Adds padding inside the input
-    marginBottom: 15, // Adds space between inputs
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 15,
     textAlign: 'center',
     textAlignVertical: 'center',
   },
   TouchableOpacity: {
-    position: 'absolute', // Positioning over the map
+    position: 'absolute',
     alignItems: 'center',
     left: '35%',
-    top: '60%',
+    top: '40%',
     backgroundColor: '#007bff',
     paddingVertical: 10,
     paddingHorizontal: 40,
