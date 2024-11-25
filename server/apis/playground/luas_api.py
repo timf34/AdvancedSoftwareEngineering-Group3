e9fd72434ad0878c1c538ec6653ade82a0b68149
kahern7 <kahern@tcd.ie>
2024-11-25 12:03:20 +0000

    Luas API fully functional

--- server/apis/playground/luas_api.py
+++ server/apis/playground/luas_api.py
@@ -7,14 +7,13 @@ import xml.etree.ElementTree as ET
 class luasAPI:
 
     def __init__(self):
-        pass
+        self.poll_interval = 10
 
     def get(self, stop):
         # URL of Open Data
         self.stop = stop
         self.url = f'http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop={self.stop}&encrypt=false'
 
-        # poll_interval = 10
 
         # while True:
         #     # Fetch the geoJSON data from the URL
@@ -26,21 +25,21 @@ class luasAPI:
         #             print(item['name'], ' ', item['available_bikes'], (int(item['last_update'])))
 
         #     else:
-        #         print("Failed to retrieve data:", response.status_code)
+        #         print('Failed to retrieve data:', response.status_code)
 
-        #     time.sleep(poll_interval)
+        #     time.sleep(self.poll_interval)
 
-        #while True:
         # fetch the real-time Luas data from the URL
         self.response = requests.get(self.url)
 
         # check if the request was succesful
         if self.response.status_code == 200:
+
             # parse xml content
             root = ET.fromstring(self.response.content)
 
             # initialise dictionaries to store tram info by direction
-            tram_info = {"Inbound": [], "Outbound": []}
+            tram_info = {'Inbound': [], 'Outbound': []}
 
             # iterate through each direction
             for direction in root.findall('direction'):
@@ -48,11 +47,21 @@ class luasAPI:
                 for tram in direction.findall('tram'):
                     due_mins = tram.get('dueMins')
                     destination = tram.get('destination')
-                    tram_info[direction_name].append({"dueMins": due_mins, "destination": destination})
+                    tram_info[direction_name].append({'dueMins': due_mins, 'destination': destination})
 
             # print the extracted tram information
-            print("Inbound Trams:", tram_info["Inbound"])
-            print("Outbound Trams:", tram_info["Outbound"])
+            print('Inbound Trams:', tram_info['Inbound'])
+            print('Outbound Trams:', tram_info['Outbound'], '\n')
 
-luas = luasAPI()
-luas.get('ran')
+        else:
+            print('Failed to retrieve data:', self.response.status_code)
+        
+        # time.sleep(self.poll_interval)
+
+if __name__ == '__main__':
+    luas = luasAPI()
+    while True:
+        stop = str(input('Enter stop code to see live arrivals and departures, or \"exit\" to leave: '))
+        if stop == 'exit':
+            break
+        luas.get(stop)
